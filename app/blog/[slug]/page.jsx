c8ce67867269ccb0048bc38b5b0627d443f43608
baappy1 <74849165+baappy1@users.noreply.github.com>
2026-02-09 14:11:32 +0600

    Update page.jsx

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import BlogContent from "@/components/NewsAndBlog/BlogContent";
 import BlogDetails from "@/components/NewsAndBlog/BlogDetails";
 import SimilarInsights from "@/components/NewsAndBlog/SimilarInsights";
