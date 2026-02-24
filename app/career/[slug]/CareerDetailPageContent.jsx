b5bddbf3cc1f811613bb43a32f6a8ff97284f6f0
Salekin Imran <abir045@gmail.com>
2026-02-24 11:45:34 +0600

    updated footer menu alignment for lg screen

--- app/career/[slug]/CareerDetailPageContent.jsx
+++ app/career/[slug]/CareerDetailPageContent.jsx
@@ -33,7 +33,7 @@ export default async function CareerDetailPageContent({
 
   return (
     <div className="pt-[68px] pb-[80px] sm:pb-[150px] lg:pb-[350px] bg-[#F8F8F3]">
-      <div className="container lg:pr-0 lg:pl-0 pr-5 pl-5">
+      <div className="container lg:px-0 px-5">
         <div className="flex flex-wrap lg:gap-0 gap-10">
           <div className="w-full lg:w-[60%]">
             <CareerHeader
