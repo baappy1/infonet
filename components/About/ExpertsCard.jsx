becd7e7aee7c47e3d3c2fd7ebf835ce919c0dc74
Salekin Imran <abir045@gmail.com>
2026-02-25 13:51:47 +0600

    updated expert card image style

--- components/About/ExpertsCard.jsx
+++ components/About/ExpertsCard.jsx
@@ -9,7 +9,7 @@ const ExpertsCard = ({ member }) => {
           src={member.image}
           alt={member.name}
           fill
-          className="object-center rounded-lg"
+          className="object-cover object-top  rounded-lg"
         />
       </div>
       <div className="w-full">
